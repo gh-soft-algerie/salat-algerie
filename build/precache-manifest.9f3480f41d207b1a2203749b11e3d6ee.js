209fe0a01c6129f80b0bfd45b075fadbc2aa5b73
Workbeesdig <adlen025@gmail.com>
2020-08-06 23:10:15 +0100

    Production build commit

---
+++ build/precache-manifest.9f3480f41d207b1a2203749b11e3d6ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d38ea202ccf396f55adf9eff586062",
+    "url": "/salat-algerie/index.html"
+  },
+  {
+    "revision": "fe2858b23c3642b1b777",
+    "url": "/salat-algerie/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "302a69af1a8f93e05f72",
+    "url": "/salat-algerie/static/js/2.3a0e4f2f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/salat-algerie/static/js/2.3a0e4f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe2858b23c3642b1b777",
+    "url": "/salat-algerie/static/js/main.10c4c9cb.chunk.js"
+  },
+  {
+    "revision": "e61f750bc9641834df09",
+    "url": "/salat-algerie/static/js/runtime-main.60763098.js"
+  }
+]);
